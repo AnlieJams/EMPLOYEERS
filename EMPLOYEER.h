f945f9a20f8334a1a6270dd87f8d046c6a57c764
AnlieJams <98762736+AnlieJams@users.noreply.github.com>
2023-05-26 13:32:45 +0300

    Update EMPLOYEER.h

--- EMPLOYEER.h
+++ EMPLOYEER.h
@@ -1,1 +1,56 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
+class Employer {
+protected:
+	string name;
+
+public:
+    Employer() {
+        name = "";
+    }
+
+    Employer(string name) {
+        this->name = name;
+    }
+
+	virtual void Show() = 0;
+};
+
+class President : public Employer {
+public:
+    President() {}
+    President(string name) {
+        this->name = name;
+    }
+
+    void Show() 
+    { 
+        cout << "President: " << name << "\n";
+    }
+};
+
+class Manager : public Employer {
+public:
+    Manager() {}
+    Manager(string name) {
+        this->name = name;
+    }
+
+    void Show() 
+    { 
+        cout << "Manager: " << name << "\n"; 
+    }
+};
+
+class Worker : public Employer {
+public:
+    Worker() {}
+
+    Worker(string name) {
+        this->name = name;
+    }
+
+    void Show() { cout << "Worker: " << name << "\n"; }
+};
